b776aba3ebfd4149d7313b688aed487cb9561dd6
nicocarcamo <nicolascarcamonaccarino@gmail.com>
2022-12-28 12:44:48 +0000

    async/await added

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 
-
 function TodoList() {
     const [todos, setTodos] = useState([]);
     const [todo, setTodo] = useState('');
@@ -12,61 +11,57 @@ function TodoList() {
         setTodo(event.target.value);
     };
 
-    const handleSubmitUser = (event) => {
+    const handleSubmitUser = async (event) => {
         event.preventDefault();
         console.log(event);
         const inputFieldValue = event.target.elements.inputField.value;
-        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${inputFieldValue}`, {
+        try {
+          const response = await fetch(`https://assets.breatheco.de/apis/fake/todos/user/${inputFieldValue}`, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
+              'Content-Type': 'application/json',
             },
             body: JSON.stringify({ todo: todo }),
-        })
-            .then(response => response.json())
-            .then(data => {
-                if (data.result) {
-                    toast.success('User Successfully Created!', {
-                        position: "top-left",
-                        autoClose: 2500,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        draggable: true,
-                        progress: undefined,
-                        theme: "light",
-                    });
-                } else {
-                    toast.error("Couldn't Create. User Already Exists!", {
-                        position: "top-left",
-                        autoClose: 2500,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        draggable: true,
-                        progress: undefined,
-                        theme: "light",
-                    });
-                }
-            })
-            .catch(error => console.log(error));
+          });
+          const data = await response.json();
+          if (data.result) {
+            toast.success('User Successfully Created!', {
+              position: "top-left",
+              autoClose: 2500,
+              hideProgressBar: false,
+              closeOnClick: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
+            });
+          } else {
+            toast.error("Couldn't Create. User Already Exists!", {
+              position: "top-left",
+              autoClose: 2500,
+              hideProgressBar: false,
+              closeOnClick: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
+            });
+          }
+        } catch (error) {
+          console.log(error);
+        }
         event.target.reset();
-    };
-
-
-    const handleGetUser = (event) => {
+      };
+      
+      const handleGetUser = async (event) => {
         event.preventDefault();
         console.log(event);
         const inputFieldValue = event.target.elements.inputField.value;
         setUsername(inputFieldValue);
-        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${inputFieldValue}`)
-          .then(response => {
-            if (response.ok) {
-              return response.json();
-            }
-            throw new Error(response.statusText);
-          })
-          .then(data => {
+        try {
+          const response = await fetch(`https://assets.breatheco.de/apis/fake/todos/user/${inputFieldValue}`);
+          if (response.ok) {
+            const data = await response.json();
             setTodos(data);
-            toast.success('User Successfully Fetched from the Server!', {
+            toast.success("User & Todo's Successfully Fetched From The Server!", {
               position: "top-left",
               autoClose: 2500,
               hideProgressBar: false,
@@ -75,110 +70,113 @@ function TodoList() {
               progress: undefined,
               theme: "light"
             });
-          })
-          .catch(error => {
-            console.log(error);
-            toast.error("Couldn't fetch. User Doesn't Exist!", {
+          } else {
+            throw new Error(response.statusText);
+          }
+        } catch (error) {
+          console.log(error);
+          toast.error("Couldn't fetch. User Doesn't Exist!", {
+            position: "top-left",
+            autoClose: 2500,
+            hideProgressBar: false,
+            draggable: true,
+            progress: undefined,
+            theme: "light"
+          });
+        }
+        event.target.reset();
+      };
+      
+            
+      const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (todo.trim() !== '') {
+          try {
+            const response = await fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`);
+            const data = await response.json();
+            const newTodos = [...data, { label: todo, done: false }];
+            const putResponse = await fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
+              method: 'PUT',
+              body: JSON.stringify(newTodos),
+              headers: {
+                'Content-Type': 'application/json'
+              }
+            });
+            console.log(putResponse)
+            toast.success('Todo List Updated Successfully!', {
               position: "top-left",
-              autoClose: 2500,
+              autoClose: 1500,
               hideProgressBar: false,
+              closeOnClick: true,
               draggable: true,
               progress: undefined,
               theme: "light"
             });
-          });
-        event.target.reset();
-      };
-            
-      
-      
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (todo.trim() !== '') {
-            fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`)
-                .then(response => response.json())
-                .then(data => {
-                    const newTodos = [...data, { label: todo, done: false }];
-                    fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
-                        method: 'PUT',
-                        body: JSON.stringify(newTodos),
-                        headers: {
-                            'Content-Type': 'application/json'
-                        }
-                    })
-                        .then(response => response.json())
-                        .then(data => {
-                            toast.success('Todo List Updated Successfully!', {
-                                position: "top-left",
-                                autoClose: 1500,
-                                hideProgressBar: false,
-                                closeOnClick: true,
-                                draggable: true,
-                                progress: undefined,
-                                theme: "light"
-                              });
-                            setTodos(newTodos);
-                            setTodo('');
-                        })
-                        .catch(error => console.log(error));
-                })
-                .catch(error => console.log(error));
+            setTodos(newTodos);
+            setTodo('');
+          } catch (error) {
+            console.log(error);
+          }
         }
-    };
+      };
+      
 
-    const handleDelete = (index) => {
+      const handleDelete = async (index) => {
         const newTodos = todos.filter((_, i) => i !== index);
         setTodos(newTodos);
-        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
+        try {
+          const response = await fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
             method: 'PUT',
             body: JSON.stringify(newTodos),
             headers: {
-                'Content-Type': 'application/json'
+              'Content-Type': 'application/json'
             }
-        })
-            .then(response => response.json())
-            .then(data => {
-                if (data.result) {
-                    toast.success('Todo List Updated Successfully!', {
-                        position: "top-left",
-                        autoClose: 1500,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        draggable: true,
-                        progress: undefined,
-                        theme: "light"
-                      });
-                } else {
-                    console.error('Error updating todo list:', data.message);
-                }
-            })
-            .catch(error => console.log(error));
-    };
+          });
+          const data = await response.json();
+          if (data.result) {
+            toast.success('Todo List Updated Successfully!', {
+              position: "top-left",
+              autoClose: 1500,
+              hideProgressBar: false,
+              closeOnClick: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light"
+            });
+          } else {
+            console.error('Error updating todo list:', data.message);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      
 
-    const handleDeleteAll = () => {
-        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
+      const handleDeleteAll = async () => {
+        try {
+          const response = await fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
             method: 'DELETE'
-        })
-            .then(response => response.json())
-            .then(data => {
-                if (data.result === 'ok') {
-                    toast.success('User Successfully Deleted!', {
-                        position: "top-left",
-                        autoClose: 2500,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        draggable: true,
-                        progress: undefined,
-                        theme: "light"
-                      });
-                    setTodos([]);
-                } else {
-                    console.error('Error deleting user and todo list:', data.message);
-                }
-            })
-            .catch(error => console.log(error));
-    };
+          });
+          const data = await response.json();
+          if (data.result === 'ok') {
+            toast.success("User & Todo's Successfully Deleted!", {
+              position: "top-left",
+              autoClose: 2500,
+              hideProgressBar: false,
+              closeOnClick: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light"
+            });
+            setTodos([]);
+          } else {
+            console.error('Error deleting user and todo list:', data.message);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      
 
     return (
         <div className="container my-5">
@@ -219,7 +217,6 @@ function TodoList() {
             <button type="button" className="btn btn-danger mt-3 mb-5" onClick={handleDeleteAll}>
                 Delete User & Todos
             </button>
-
             <form className="d-flex my-2" onSubmit={handleSubmitUser}>
                 <input className="form-control user-input" type="text" name="inputField" id="inputField" placeholder="Enter new username..." />
                 <button className="btn btn-secondary btn-sm ms-2" type="submit">Create New User</button>
@@ -239,8 +236,6 @@ function TodoList() {
                 draggable
                 theme="light"
             />
-
-
         </div>
     );
 }
